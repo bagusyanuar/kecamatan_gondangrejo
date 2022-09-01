81f4b1c36a5248570726aeeb7a4b97ef39beca4a
bagusyanuar <bagus.yanuar613@gmail.com>
2022-09-01 21:59:59 +0700

    oke

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,11 +51,60 @@
         </div>
     </section>
 
-    <section class="container-fluid" id="desa-gondangrejo" style="margin-top: 6rem">
+    <section class="container-fluid" id="desa-gondangrejo" style="margin-top: 3rem">
         <p style="font-weight: 300; color: #636363;" class="text-center f14 container">
             Jumlah Penduduk Se – Kecamatan Gondangrejo adalah 69.897 Jiwa Yang Terbagi Menjadi 13 Desa.
         </p>
     </section>
+    <section class="container-fluid mt-5 text-center pt-5 pb-5"
+             style="position: relative; background-image: url('../assets/images/aduan.jpg'); background-repeat: no-repeat; background-size: cover;">
+        <div class="cover-black-all"></div>
+        <div class="d-flex justify-content-center align-items-center h-100 flex-column front">
+            <div class="pl-5 pr-5">
+                <a class="sukmatrip font-weight-bold" style="color: var(--accentColor);">ADUAN MASYARAKAT</a>
+                <hr class="mb-5" style="z-index: 3; width: 5rem; border-top: 1px solid whitesmoke">
+            </div>
+            <div class="row justify-content-center w-100">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="font-weight-bold f16">
+                                Form Pengaduan
+                            </p>
+                            <hr>
+                            <form method="post" action="/pengaduan/create">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="nama" class="text-left w-100">Nama</label>
+                                    <input type="text" class="form-control form-data-input" id="nama"
+                                           aria-describedby="namaHelp" placeholder="Nama" name="nama" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="no_hp" class="text-left w-100">No. Handphone / Whatsapp</label>
+                                    <input type="number" class="form-control form-data-input" id="no_hp"
+                                           aria-describedby="namaHelp" placeholder="Nama" name="no_hp" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="pengaduan" class="text-left w-100">Aduan</label>
+                                    <textarea rows="3" class="form-control form-data-input" id="pengaduan"
+                                              aria-describedby="aduan" placeholder="Isi Pengaduan" name="pengaduan"
+                                              required></textarea>
+                                </div>
+                                <hr>
+                                <div class="text-right w-100">
+                                    <button type="submit" class="btn bt-booknow">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <div class="container">
+        <hr style="border-color: var(--accentColor); margin-top: 7em;" class="">
+    </div>
     <section class="container">
         <div class="text-center mt-5 mb-5">
             <a class="sukmatrip" style="color: black">DESA DI GONDANGREJO</a>
@@ -195,50 +244,7 @@
             </div>
         </div>
     </section>
-    <hr style="border-color: var(--accentColor); margin-top: 7em;" class="container">
-    <section class="container-fluid mt-5 text-center p-5"
-             style="position: relative; background-image: url('../assets/images/aduan.jpg'); background-repeat: no-repeat; background-size: cover;">
-        <div class="cover-black-all"></div>
-        <div class="d-flex justify-content-center align-items-center h-100 flex-column front">
-            <a class="sukmatrip font-weight-bold" style="color: var(--accentColor);">ADUAN MASYARAKAT</a>
-            <hr class="mb-5" style="z-index: 3; width: 5rem; border-top: 1px solid whitesmoke">
-            <div class="row justify-content-center w-100">
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <p class="font-weight-bold f16">
-                                Form Pengaduan
-                            </p>
-                            <hr>
-                            <form method="post" action="/pengaduan/create">
-                                @csrf
-                                <div class="form-group">
-                                    <label for="nama" class="text-left w-100">Nama</label>
-                                    <input type="text" class="form-control form-data-input" id="nama"
-                                           aria-describedby="namaHelp" placeholder="Nama" name="nama" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="no_hp" class="text-left w-100">No. Handphone / Whatsapp</label>
-                                    <input type="number" class="form-control form-data-input" id="no_hp"
-                                           aria-describedby="namaHelp" placeholder="Nama" name="no_hp" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="pengaduan" class="text-left w-100">Aduan</label>
-                                    <textarea rows="3" class="form-control form-data-input" id="pengaduan"
-                                              aria-describedby="aduan" placeholder="Isi Pengaduan" name="pengaduan"
-                                              required></textarea>
-                                </div>
-                                <hr>
-                                <div class="text-right w-100">
-                                    <button type="submit" class="btn bt-booknow">Submit</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </section>
-    <hr style="border-color: var(--accentColor); margin-top: 3em; margin-bottom: 3em" class="container">
+    <div class="container">
+        <hr style="border-color: var(--accentColor); margin-top: 3em; margin-bottom: 3em" class="">
+    </div>
 @endsection
